9f99c1ee3d9cb1696f63d455823fa4747ca020c1
Bavanuja <bbavanuja@gmail.com>
2024-07-24 21:15:24 +0530

    socialmedia icons

--- src/App.js
+++ src/App.js
@@ -13,12 +13,19 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { lightTheme, darkTheme } from './theme';
+import Link from '@mui/material/Link';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 
 function Nav({ darkMode, handleThemeChange }) {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-
+  const handleReload = (e) => {
+    e.preventDefault(); // Prevent default link behavior
+    window.location.reload(); // Reload the page
+  };
   return (
     <Toolbar>
       {isMobile && (
@@ -26,11 +33,15 @@ function Nav({ darkMode, handleThemeChange }) {
           <MenuIcon />
         </IconButton>
       )}
-      <Typography variant="h4" component="div" className="logo" flexGrow={1} sx={{ fontFamily: "Pacifico", color: "purple", fontWeight: "200" }}>
+      <Link href="/" onClick={handleReload} underline="none" flexGrow={1} >
+      <Typography variant="h4" component="div" className="logo" sx={{ fontFamily: "Pacifico", color: "purple", fontWeight: "200" }}>
+      
         <span>&lt;</span>
         Bavanuja
         <span>/&gt;</span>
-      </Typography>
+        
+        </Typography>
+        </Link>
       {!isMobile && (
         <>
           <Button color="inherit" sx={{ padding: 3 }}>Skills</Button>
@@ -63,22 +74,36 @@ function AboutSection() {
         justifyContent: 'center',
         color: 'white', // Adjust text color as needed
         padding: 2,
+       
       }}
     >
       <Container >
-        <Typography variant="h2" sx={{ mb: 2,  fontFamily: "Pacifico" ,}}>
-          Hi, I'm Bavanuja Aspiring Frontend Developer
+        <Typography variant="h2" sx={{ mb:2,  fontFamily: "Pacifico" ,}}>
+          Hi, I'm Bavanuja 
         </Typography>
-        <Typography>
+        <Typography variant="h3" sx={{mb:2,fontFamily:  "Pacifico" ,}}>
         Aspiring Frontend Developer
         </Typography>
-        <Typography variant="body1" sx={{ fontSize: '1rem', lineHeight: 1.5, }}>
-          Hi, I'm [Your Name], a [Your Profession or Field]. I have a passion for [Your Passion] and enjoy working on [Your Interests]. 
-          My journey in [Your Field] has been [Brief Description of Your Journey], and I am excited to continue growing and learning. 
-          Here, you can find some of the projects I've worked on and learn more about what drives me.
-        </Typography>
+        <SocialMediaLinks />
+        <Button variant="contained"   sx={{color:"white",fontWeight:"700"}}>Get Resume</Button>
         </Container>
     </Box>
+  );
+}
+
+function SocialMediaLinks() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'left', gap: '10px',mb:4}}>
+      <IconButton component="a" href="https://github.com/your-username" target="_blank" rel="noopener noreferrer">
+        <GitHubIcon fontSize="large" sx={{ color: '#181717' }}/>
+      </IconButton>
+      <IconButton component="a" href="https://www.linkedin.com/in/your-username/" target="_blank" rel="noopener noreferrer">
+        <LinkedInIcon fontSize="large" sx={{ color: '#0A66C2' }}/>
+      </IconButton>
+      <IconButton component="a" href="https://www.facebook.com/your-username" target="_blank" rel="noopener noreferrer">
+        <FacebookIcon fontSize="large" sx={{ color: '#1877F2' }} />
+      </IconButton>
+    </div>
   );
 }
 
